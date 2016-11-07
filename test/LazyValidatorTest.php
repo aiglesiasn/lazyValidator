cabf027e1109263b6f60f64565e726dd06794849
trzczy <trzczy@gmail.com>
2016-11-07 17:33:02 +0100

    code climate configuration

--- test/LazyValidatorTest.php
+++ test/LazyValidatorTest.php
@@ -12,6 +12,108 @@ class LazyValidatorTest extends TestCase
     public function initialParametersAndResult()
     {
         return [
+            //test data 1
+            //post data 1
+            //methods 1
+            [
+                /*post data*/
+                '{
+                    "username": "",
+                    "email": "abc@domain.ko",
+                    "password": "adam",
+                    "confirm": ""
+                }',
+
+                /*groups of validation methods and their parameters*/
+                [
+                    //methods group
+                    json_decode
+                    ('[
+                            {
+                                "method": "length",
+                                "input": "username",
+                                "min": 2,
+                                "max": 3,
+                                "message": "Username must be between 2 and 3 characters long."
+                            },
+                            {
+                                "method": "length",
+                                "input": "username",
+                                "min": 2,
+                                "max": 8,
+                                "message": "Username must be between 2 and 8 characters long."
+                            },
+                            {
+                                "method": "regex",
+                                "input": "username",
+                                "pattern": "^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$",
+                                "message": "Username may contain letters, numbers, spaces, hyphens and underscores."
+                            },
+                            {
+                                "method": "email",
+                                "input": "email",
+                                "message": "Email address format not proper."
+                            },
+                            {
+                                "method": "length",
+                                "input": "password",
+                                "min": 3,
+                                "max": 4096,
+                                "message": "Password must be longer than 2 characters."
+                            },
+                            {
+                                "method": "confirm",
+                                "input": "confirm",
+                                "message": "Passwords must be the same."
+                            }
+                        ]
+                    ', true),
+                    //methods group
+                    json_decode
+                    ('[
+                            {
+                                "method": "unique",
+                                "input": "email",
+                                "table": "email",
+                                "message": "Email address must be unique."
+                            }
+                        ]
+                    ', true),
+                    //methods group
+                    json_decode
+                    ('[
+                            {
+                                "method": "unique",
+                                "input": "username",
+                                "table": "username",
+                                "message": "Username must be unique."
+                            }
+                        ]
+                    ', true)
+                ],
+
+                /*validation methods class name*/
+                'ValidationMethods1',
+
+                /*result*/
+                '[
+                    {
+                        "failedInput": "username",
+                        "messages": [
+                            "Username must be between 2 and 3 characters long.",
+                            "Username must be between 2 and 8 characters long.",
+                            "Username may contain letters, numbers, spaces, hyphens and underscores."
+                        ]
+                    },
+                    {
+                        "failedInput": "confirm",
+                        "messages": [
+                            "Passwords must be the same."
+                        ]
+                    }
+                ]'
+            ],
+
             //test data 2
             //post data 2
             //methods 1
@@ -243,6 +345,7 @@ class LazyValidatorTest extends TestCase
     {
         $fullValidationMethodsClassName = 'Trzczy\\Helpers\\' . $validationMethodsClassName;
         $validationMethodsObject = new $fullValidationMethodsClassName($this->userDaoImpl);
+        var_dump($validationMethodsObject);
         $this->validator = new LazyValidator($validationMethodsObject);
         $result = $this->validator->validate(
             json_decode($postData, true),
